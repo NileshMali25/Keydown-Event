0c77a7d9aadcb88a77b209e187d3e53243da9c48
Nilesh Supadu Mali <nilu12969@gmail.com>
2026-02-01 02:02:21 +0530

    Update h1 text on space key press

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let h1=document.querySelector("h1");
 window.addEventListener("keydown",function(details){
+    if(details.key===" "){
+        h1.textContent="SPC";
+    }
+    else{
     h1.textContent=details.key;
+    }
 });
